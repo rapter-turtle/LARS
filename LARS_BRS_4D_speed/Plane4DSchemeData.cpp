0448a5c6a77e230c57cde2a7eb6e6cc149915c2a
rapter-turtle <58213518+rapter-turtle@users.noreply.github.com>
2025-12-17 15:29:58 +0900

    Add files via upload

---
+++ LARS_BRS_4D_speed/Plane4DSchemeData.cpp
@@ -0,0 +1,159 @@
+#include "Plane4DSchemeData.hpp"
+#include <algorithm>
+#include <typeinfo>
+#include <macro.hpp>
+bool Plane4DSchemeData::operator==(const Plane4DSchemeData& rhs) const {
+	if (this == &rhs) return true;
+	else if (!SchemeData::operator==(rhs)) return false;
+	else if (wMax != rhs.wMax) return false;
+	else if ((aranges.size() != rhs.aranges.size()) || !std::equal(aranges.cbegin(), aranges.cend(), rhs.aranges.cbegin())) return false;
+	else return true;
+}
+bool Plane4DSchemeData::operator==(const SchemeData& rhs) const {
+	if (this == &rhs) return true;
+	else if (typeid(*this) != typeid(rhs)) return false;
+	else return operator==(dynamic_cast<const Plane4DSchemeData&>(rhs));
+}
+
+bool Plane4DSchemeData::hamFunc(
+	beacls::UVec& hamValue_uvec,
+	const FLOAT_TYPE,
+	const beacls::UVec&,
+	const std::vector<beacls::UVec>& derivs,
+	const size_t begin_index,
+	const size_t length
+	) const {
+	const levelset::HJI_Grid *hji_grid = get_grid();
+
+	const beacls::FloatVec &xs0 = hji_grid->get_xs(0);
+	const beacls::FloatVec &xs1 = hji_grid->get_xs(1);
+	const beacls::FloatVec &xs2 = hji_grid->get_xs(2);
+	const beacls::FloatVec &xs3 = hji_grid->get_xs(3);
+	beacls::reallocateAsSrc(hamValue_uvec, derivs[0]);
+	FLOAT_TYPE* hamValue = beacls::UVec_<FLOAT_TYPE>(hamValue_uvec).ptr();
+	const FLOAT_TYPE* deriv0 = beacls::UVec_<FLOAT_TYPE>(derivs[0]).ptr();
+	const FLOAT_TYPE* deriv1 = beacls::UVec_<FLOAT_TYPE>(derivs[1]).ptr();
+	const FLOAT_TYPE* deriv2 = beacls::UVec_<FLOAT_TYPE>(derivs[2]).ptr();
+	const FLOAT_TYPE* deriv3 = beacls::UVec_<FLOAT_TYPE>(derivs[3]).ptr();
+
+	for (size_t i = 0; i<length; ++i) {
+		FLOAT_TYPE deriv0_i = deriv0[i];
+		FLOAT_TYPE deriv1_i = deriv1[i];
+		FLOAT_TYPE deriv2_i = deriv2[i];
+		FLOAT_TYPE deriv3_i = deriv3[i];
+		FLOAT_TYPE xs0_i = xs0[begin_index + i];
+		FLOAT_TYPE xs1_i = xs1[begin_index + i];
+		FLOAT_TYPE xs2_i = xs2[begin_index + i];
+		FLOAT_TYPE xs3_i = xs3[begin_index + i];
+		FLOAT_TYPE V0 = 3.0;
+		FLOAT_TYPE Kp = 1.5;
+		FLOAT_TYPE dx = 0.4;
+		FLOAT_TYPE dy = 0.15;
+		FLOAT_TYPE dpsi = 0.15;
+		FLOAT_TYPE alpha_max = 0.4;
+			
+		// hamValue[i] = -((V0*std::cos(xs2_i)) * deriv0_i - dx*HjiFabs(deriv0_i)
+		// + (V0*std::sin(xs2_i)) * deriv1_i - dy*HjiFabs(deriv1_i)
+		// + xs3_i*deriv2_i - dpsi*HjiFabs(deriv2_i)
+		// + alpha_max*HjiFabs(deriv3_i));	
+
+		// hamValue[i] = -((Kp*std::sqrt(xs0_i*xs0_i + xs1_i*xs1_i)*std::cos(xs2_i)) * deriv0_i - dx*HjiFabs(deriv0_i)
+		// + (Kp*std::sqrt(xs0_i*xs0_i + xs1_i*xs1_i)*std::sin(xs2_i)) * deriv1_i - dy*HjiFabs(deriv1_i)
+		// + xs3_i*deriv2_i - dpsi*HjiFabs(deriv2_i)
+		// + alpha_max*HjiFabs(deriv3_i));	
+
+		hamValue[i] = -((-Kp*xs0_i*std::cos(xs2_i)) * deriv0_i - dx*HjiFabs(deriv0_i)
+		+ (Kp*xs0_i*std::sin(xs2_i)) * deriv1_i - dy*HjiFabs(deriv1_i)
+		+ xs3_i*deriv2_i - dpsi*HjiFabs(deriv2_i)
+		+ alpha_max*HjiFabs(deriv3_i));				
+		
+	}
+	return true;
+}
+bool Plane4DSchemeData::partialFunc(
+	beacls::UVec& alphas_uvec,
+	const FLOAT_TYPE,
+	const beacls::UVec&,
+	const std::vector<beacls::UVec>&,
+	const std::vector<beacls::UVec>&,
+	const size_t dim,
+	const size_t begin_index,
+	const size_t length
+) const {
+	if (alphas_uvec.type() != beacls::UVecType_Vector) alphas_uvec = beacls::UVec(beacls::type_to_depth<FLOAT_TYPE>(), beacls::UVecType_Vector, length);
+	else alphas_uvec.resize(length);
+	FLOAT_TYPE* alphas = beacls::UVec_<FLOAT_TYPE>(alphas_uvec).ptr();
+	const levelset::HJI_Grid *hji_grid = get_grid();
+	switch (dim) {
+	case 0:
+	{
+		FLOAT_TYPE V0 = 3.0;
+		FLOAT_TYPE Kp = 1.5;
+		FLOAT_TYPE dx = 0.4;
+		FLOAT_TYPE dy = 0.15;
+		FLOAT_TYPE dpsi = 0.15;
+		FLOAT_TYPE alpha_max = 0.4;
+		const beacls::FloatVec &xs0 = hji_grid->get_xs(0);
+		const beacls::FloatVec &xs1 = hji_grid->get_xs(1);
+		const beacls::FloatVec &xs2 = hji_grid->get_xs(2);
+		for (size_t i = 0; i<length; ++i) {
+			// alphas[i] = HjiFabs(V0*std::cos(xs2[i+ begin_index]) - dx);
+			// alphas[i] = HjiFabs(Kp*std::sqrt(xs0[i+ begin_index]*xs0[i+ begin_index] + xs1[i+ begin_index]*xs1[i+ begin_index])*std::cos(xs2[i+ begin_index]) - dx);
+			alphas[i] = HjiFabs(-Kp*xs0[i+ begin_index]*std::cos(xs2[i+ begin_index]) - dx);
+			
+
+		}
+	}
+	break;
+	case 1:
+	{
+		FLOAT_TYPE V0 = 3.0;
+		FLOAT_TYPE Kp = 1.5;
+		FLOAT_TYPE dx = 0.4;
+		FLOAT_TYPE dy = 0.15;
+		FLOAT_TYPE dpsi = 0.15;
+		FLOAT_TYPE alpha_max = 0.4;
+		const beacls::FloatVec &xs0 = hji_grid->get_xs(0);
+		const beacls::FloatVec &xs1 = hji_grid->get_xs(1);
+		const beacls::FloatVec &xs2 = hji_grid->get_xs(2);
+		for (size_t i = 0; i<length; ++i) {
+			// alphas[i] = HjiFabs(V0*std::sin(xs2[i+ begin_index]) - dy);
+			alphas[i] = HjiFabs(-Kp*xs0[i+ begin_index]*std::sin(xs2[i+ begin_index]) - dy);
+		}
+	}
+	break;
+	case 2:
+	{
+		FLOAT_TYPE V0 = 3.0;
+		FLOAT_TYPE Kp = 1.5;
+		FLOAT_TYPE dx = 0.4;
+		FLOAT_TYPE dy = 0.15;
+		FLOAT_TYPE dpsi = 0.15;
+		FLOAT_TYPE alpha_max = 0.4;
+		const beacls::FloatVec &xs0 = hji_grid->get_xs(0);
+		const beacls::FloatVec &xs1 = hji_grid->get_xs(1);
+		const beacls::FloatVec &xs3 = hji_grid->get_xs(3);
+		for (size_t i = 0; i<length; ++i) {
+			alphas[i] = HjiFabs(xs3[begin_index + i] - dpsi);
+		}
+	}
+	break;
+	case 3:
+	{
+		FLOAT_TYPE V0 = 3.0;
+		FLOAT_TYPE Kp = 1.5;
+		FLOAT_TYPE dx = 0.4;
+		FLOAT_TYPE dy = 0.15;
+		FLOAT_TYPE dpsi = 0.15;
+		FLOAT_TYPE alpha_max = 0.4;
+		for (size_t i = 0; i<length; ++i) {
+			alphas[i] = alpha_max;
+		}
+	}
+	break;
+	default:
+		printf("error:%s,%dPartials for the game of two identical vehicles only exist in dimensions 1-3\n", __func__, __LINE__);
+		return false;
+	}
+	return true;
+}
